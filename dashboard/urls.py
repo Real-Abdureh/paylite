f82dee0fddc02ec9bf09772150b0ab0944eece47
Real-Abdureh <abdouliabbas75@gmail.com>
2023-01-16 17:06:20 +0100

    change routing

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('index',views.index, name='index'),
     path('home/',views.home, name='customer-home'),
     path('pay/', views.pay, name= 'customer-pay'  ),
-    path('landing/', views.landing, name='landing-page'),
+    path('', views.landing, name='landing-page'),
     # path('check/', views.CustomerListView.as_view, name='checking')
     path('receipt/', views.generate_receipt, name='sample-receipt'),
    
